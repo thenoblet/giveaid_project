b21fc0e6a41b0604e23687159fe80ebeaea382f6
noblet <appiahpatricknoblet@gmail.com>
2024-06-24 16:48:14 +0100

    updated settings.py to take in the installed ups

--- giveaid_project/settings.py
+++ giveaid_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'giveaid.apps.GiveaidConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
